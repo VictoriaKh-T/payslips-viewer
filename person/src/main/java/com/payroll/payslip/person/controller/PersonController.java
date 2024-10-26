b4f000d42dd6ff3fd8a00546d1644b6310574e25
VictoriaKh-T <kaplabetty@gmail.com>
2024-10-26 21:23:21 +0200

    some changes

--- person/src/main/java/com/payroll/payslip/person/controller/PersonController.java
+++ person/src/main/java/com/payroll/payslip/person/controller/PersonController.java
@@ -2,11 +2,6 @@ package com.payroll.payslip.person.controller;
 
 import java.time.LocalDate;
 import java.util.List;
-import com.payroll.payslip.person.model.dto.CreatePersonRequest;
-import com.payroll.payslip.person.model.dto.CreatePersonResponse;
-import com.payroll.payslip.person.model.dto.PersonResponse;
-import com.payroll.payslip.person.model.dto.UpdatePersonRequest;
-import com.payroll.payslip.person.model.dto.UpdatePersonResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.payroll.payslip.person.model.dto.CreatePersonRequest;
+import com.payroll.payslip.person.model.dto.CreatePersonResponse;
+import com.payroll.payslip.person.model.dto.PersonResponse;
+import com.payroll.payslip.person.model.dto.UpdatePersonRequest;
+import com.payroll.payslip.person.model.dto.UpdatePersonResponse;
 import com.payroll.payslip.person.service.PersonService;
 
 @Tag(name = "User management", description = "Endpoints for managing organization")
