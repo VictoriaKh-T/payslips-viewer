e09e26b0b065ad2927c4e5f345190e29caa75de6
Victoriakh_T <kaplabetty@gmail.com>
2024-11-16 20:28:55 +0100

    grpc-server spotless

--- gRPC-server/src/main/java/com/payroll/payslip/grpc/server/EmployeeDataServiceImpl.java
+++ gRPC-server/src/main/java/com/payroll/payslip/grpc/server/EmployeeDataServiceImpl.java
@@ -1,34 +1,32 @@
 package com.payroll.payslip.grpc.server;
 
-import com.payroll.payslip.proto.Employee;
-import com.payroll.payslip.proto.GetPersonResponse;
-import com.payroll.payslip.proto.PersonDataServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import lombok.AllArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+
 import com.payroll.payslip.proto.CreateEmployeeResponse;
 import com.payroll.payslip.proto.EmployeeDataServiceGrpc;
 import com.payroll.payslip.proto.GetEmployeeRequest;
 import com.payroll.payslip.proto.GetEmployeeResponse;
 import com.payroll.payslip.proto.GetOrganizationRequest;
 import com.payroll.payslip.proto.GetPersonRequest;
+import com.payroll.payslip.proto.GetPersonResponse;
+import com.payroll.payslip.proto.PersonDataServiceGrpc;
 
 @GrpcService
 @AllArgsConstructor
 public class EmployeeDataServiceImpl extends EmployeeDataServiceGrpc.EmployeeDataServiceImplBase {
 
-    private final ManagedChannel personChannel = ManagedChannelBuilder.forAddress("localhost", 50053)
-            .usePlaintext()
-            .build();
-    private final PersonDataServiceGrpc.PersonDataServiceBlockingStub personStub
-            = PersonDataServiceGrpc.newBlockingStub(personChannel);
+  private final ManagedChannel personChannel =
+      ManagedChannelBuilder.forAddress("localhost", 50053).usePlaintext().build();
+  private final PersonDataServiceGrpc.PersonDataServiceBlockingStub personStub =
+      PersonDataServiceGrpc.newBlockingStub(personChannel);
 
   @Override
   public void createEmployeeViaPerson(
-          GetPersonRequest request,
-          StreamObserver<CreateEmployeeResponse> responseObserver) {
+      GetPersonRequest request, StreamObserver<CreateEmployeeResponse> responseObserver) {
 
     int personId = request.getPersonId();
     System.out.println("Received request for person " + personId);
@@ -39,16 +37,16 @@ public class EmployeeDataServiceImpl extends EmployeeDataServiceGrpc.EmployeeDat
     String sureName = personResponse.getPerson().getSureName();
     String fullName = sureName + " " + firstName;
 
-      CreateEmployeeResponse response = CreateEmployeeResponse.newBuilder()
-              .setId(generateEmployeeId())
-              .setFullName(fullName)
-              .setPersonId(personId)
-              .build();
+    CreateEmployeeResponse response =
+        CreateEmployeeResponse.newBuilder()
+            .setId(generateEmployeeId())
+            .setFullName(fullName)
+            .setPersonId(personId)
+            .build();
 
-      responseObserver.onNext(response);
-      responseObserver.onCompleted();
-
- }
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
 
   @Override
   public void addEmployeeOrganizationData(
@@ -62,7 +60,7 @@ public class EmployeeDataServiceImpl extends EmployeeDataServiceGrpc.EmployeeDat
     super.getEmployeeById(request, responseObserver);
   }
 
-    private int generateEmployeeId() {
-        return (int) (Math.random() * 10000);
-    }
+  private int generateEmployeeId() {
+    return (int) (Math.random() * 10000);
+  }
 }
--- gRPC-server/src/main/java/com/payroll/payslip/grpc/server/PersonDataServiceImpl.java
+++ gRPC-server/src/main/java/com/payroll/payslip/grpc/server/PersonDataServiceImpl.java
@@ -2,6 +2,7 @@ package com.payroll.payslip.grpc.server;
 
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+
 import com.payroll.payslip.proto.GetPersonRequest;
 import com.payroll.payslip.proto.GetPersonResponse;
 import com.payroll.payslip.proto.PersonDataServiceGrpc;
